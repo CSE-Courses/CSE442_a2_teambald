9160de657095c4a1ce41894d2eac3f2e2f5eb1de
enhaozhe <33393540+enhaozhe@users.noreply.github.com>
2020-09-21 00:32:18 -0400

    Feat: Added basic UI for Home page and RecordingList page.

---
+++ app/src/main/java/com/teambald/cse442_project_team_bald/HomeFragment.java
@@ -0,0 +1,39 @@
+package com.teambald.cse442_project_team_bald;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.home_fragment, container, false);
+    }
+    @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.white));
+        TextView textViewCounter = view.findViewById(R.id.tv_counter);
+        textViewCounter.setText("Home");
+    }
+}
---
+++ app/src/main/java/com/teambald/cse442_project_team_bald/RecordingListFragment.java
@@ -0,0 +1,36 @@
+package com.teambald.cse442_project_team_bald;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+public class RecordingListFragment extends Fragment {
+    public RecordingListFragment() {}
+
+    public static RecordingListFragment newInstance() {
+        return new RecordingListFragment();
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.recording_list_fragment, container, false);
+    }
+    @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.white));
+        TextView textViewCounter = view.findViewById(R.id.tv_counter);
+        textViewCounter.setText("Recording List");
+    }
+}
