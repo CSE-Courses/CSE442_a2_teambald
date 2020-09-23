bce30acfbc257d2963885f7aaef98e14253dc2f9
xuanhuaz <kaowofeixiang@gmail.com>
2020-09-23 18:48:39 -0400

    Recording_Tested

--- app/src/main/java/com/teambald/cse442_project_team_bald/HomeFragment.java
+++ app/src/main/java/com/teambald/cse442_project_team_bald/HomeFragment.java
@@ -1,21 +1,54 @@
 package com.teambald.cse442_project_team_bald;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
 public class HomeFragment extends Fragment {
+
+    private ImageButton recorderButton;
+    private boolean isRecording;
+    private MediaPlayer mediaPlayer = null;
+    private String recordPermission = Manifest.permission.RECORD_AUDIO;
+    private int PERMISSION_CODE = 21;
+    private String fileToPlay;
+    private MediaRecorder mediaRecorder;
+    private String recordFile;
+
+    private Chronometer timer;
+
     private static final String TAG = "HOME_FRAGMENT: ";
 
     public HomeFragment() {
@@ -29,22 +62,102 @@ public class HomeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.home_fragment, container, false);
     }
+
+
     @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        checkPermissions();
         view.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.white));
-        ImageButton recorderButton = view.findViewById(R.id.recorder_button);
+        recorderButton = view.findViewById(R.id.recorder_button);
+        isRecording= false;
+
         recorderButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                //TODO: @Xuanhua: Implement record function here.
-                Log.i(TAG, "Record button is clicked.");
+                if (isRecording) {
+                    //Stop Recording
+                    recorderButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_recorder_icon_150, null));
+                    stopRecording();
+                    isRecording = false;
+                } else {
+                    //Check permission to record audio
+                        //Start Recording
+                        startRecording();
+                        recorderButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_pause_button, null));
+                        isRecording = true;
+
+                }
             }
         });
+
+
+
     }
+
+    private void startRecording() {
+
+        //Get app external directory path
+        String recordPath = getActivity().getExternalFilesDir("/").getAbsolutePath();
+
+        //initialize filename variable with date and time at the end to ensure the new file wont overwrite previous file
+        recordFile = "Recording_"+"test"+ ".3gp";
+        //Setup Media Recorder for recording
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setOutputFile(recordPath + "/" + recordFile);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Start Recording
+        mediaRecorder.start();
+    }
+
+    private boolean checkPermissions() {
+        //Check permission
+        if (ActivityCompat.checkSelfPermission(getContext(), recordPermission) == PackageManager.PERMISSION_GRANTED) {
+            //Permission Granted
+            return true;
+        } else {
+            //Permission not granted, ask for permission
+            ActivityCompat.requestPermissions(getActivity(), new String[]{recordPermission}, PERMISSION_CODE);
+            return false;
+        }
+    }
+    public void onStop() {
+        super.onStop();
+        if(isRecording){
+            stopRecording();
+        }
+    }
+
+    private void stopRecording() {
+
+        //Change text on page to file saved
+        //Stop media recorder and set it to null for further use to record new audio
+        mediaRecorder.stop();
+        mediaRecorder.release();
+        mediaRecorder = null;
+
+    }
+
+
+
+
+
 }
