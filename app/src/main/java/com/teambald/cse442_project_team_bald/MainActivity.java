ac867d89321d63d7391172491bd2e759346616b0
Baicheng Chen <baicheng@buffalo.edu>
2020-09-26 19:19:22 -0400

    Added CloudFragment
    Added cloud fragment UI to view recordings in cloud.

--- app/src/main/java/com/teambald/cse442_project_team_bald/MainActivity.java
+++ app/src/main/java/com/teambald/cse442_project_team_bald/MainActivity.java
@@ -1,38 +1,56 @@
 package com.teambald.cse442_project_team_bald;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
-import android.view.View;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.teambald.cse442_project_team_bald.TabsController.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    ViewPager2 viewPager;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
+        //
+        viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(createTabAdapter());
+
+        tabLayout = findViewById(R.id.tabs);
+
+        new TabLayoutMediator(tabLayout, viewPager,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        switch(position){
+                            case 0:
+                                tab.setText("Home");
+                                tab.setIcon(R.drawable.ic_home_icon);
+                                break;
+                            case 1:
+                                tab.setText("Recordings");
+                                tab.setIcon(R.drawable.ic_recording_list_icon);
+                                break;
+                            case 2:
+                                tab.setText("Cloud");
+                                tab.setIcon(R.drawable.ic_cloud_icon);
+                                break;
+                            default:
+                                tab.setText("Home");
+                                tab.setIcon(R.drawable.ic_home_icon);
+                        }
+
+                    }
+                }).attach();
     }
 
-    private void updateUI(GoogleSignInAccount account) {
-    }
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.sign_in_button:
-                signIn();
-                break;
-        }
+    private ViewPagerAdapter createTabAdapter() {
+        return new ViewPagerAdapter(this);
     }
 }
