f3150bc8d1ed77aecde9c7e5c6f8616434e50caa
Baicheng Chen <baicheng@buffalo.edu>
2020-11-12 17:48:26 -0500

    task 66-sp4: Implemented biometric authentication

--- app/src/main/java/com/teambald/cse442_project_team_bald/Fragments/SettingFragment.java
+++ app/src/main/java/com/teambald/cse442_project_team_bald/Fragments/SettingFragment.java
@@ -3,6 +3,8 @@ package com.teambald.cse442_project_team_bald.Fragments;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,6 +19,9 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
@@ -25,6 +30,8 @@ import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseUser;
 import com.teambald.cse442_project_team_bald.MainActivity;
 import com.teambald.cse442_project_team_bald.R;
+
+import java.util.concurrent.Executor;
 
 public class SettingFragment extends Fragment implements View.OnClickListener {
     private SeekBar sBar;
@@ -49,7 +56,11 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
 
     public static final int[] times = new int[]{1,5,10,15,20,25,30};
 
+    private Switch autoRecordSwitch;
     private boolean autoRecord = false;
+
+    private Switch authenSwitch;
+    private boolean authenVal = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,8 +68,12 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
         //Initialize SharedPref and read set recording length from SharedPreference (default: 1).
         sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
         editor = sharedPref.edit();
+        pval = sharedPref.getInt(getString(R.string.recording_length_key), 1);
         autoRecord = sharedPref.getBoolean(getString(R.string.auto_record),false);
         Log.i(TAG, "PVAL: "+pval);
+        Log.i(TAG, "auto record: "+autoRecord);
+        authenVal = sharedPref.getBoolean(getString(R.string.biometric_authentication),false);
+        Log.d(TAG,"authenval: "+authenVal);
     }
 
     @Override
@@ -75,7 +90,6 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
         view.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.white));
 
         sBar = view.findViewById(R.id.recording_seekBar);
-        pval = sharedPref.getInt(getString(R.string.recording_length_key), 1);
 
         //Set saved value.
         sBar.setProgress(pval);
@@ -105,9 +119,9 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
             }
         });
 
-        Switch autoSwitch = view.findViewById(R.id.autoRecSwitch);
-        autoSwitch.setChecked(autoRecord);
-        autoSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        autoRecordSwitch = view.findViewById(R.id.autoRecSwitch);
+        autoRecordSwitch.setChecked(autoRecord);
+        autoRecordSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 autoRecord = isChecked;
@@ -124,6 +138,11 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
                 }
             }
         });
+
+        authenSwitch = view.findViewById(R.id.bioSwitch);
+        authenSwitch.setChecked(authenVal);
+        authenSwitch.setOnCheckedChangeListener(new bioAuthChangeListener());
+
 
         signInButton = view.findViewById(R.id.sign_in_button);
         signOutButton = view.findViewById(R.id.sign_out_button);
@@ -211,6 +230,85 @@ public class SettingFragment extends Fragment implements View.OnClickListener {
         else
         {
             updateSignInUI(null);
+        }
+    }
+    private class bioAuthChangeListener implements CompoundButton.OnCheckedChangeListener
+    {
+        @Override
+        public void onCheckedChanged(final CompoundButton buttonView, final boolean isChecked) {
+            int error = BiometricManager.from(getActivity()).canAuthenticate();
+            if (error != BiometricManager.BIOMETRIC_SUCCESS) {
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("Can't Use Biometrics")
+                        .setMessage("Error " + error)
+                        .create()
+                        .show();
+                Toast.makeText(getContext(),"Cannot use Biometrics on this device",Toast.LENGTH_SHORT).show();
+                buttonView.setChecked(false);
+                return;
+            }
+
+            Executor executor;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+                executor = getActivity().getMainExecutor();
+            } else {
+                final Handler handler = new Handler(Looper.getMainLooper());
+                executor = new Executor() {
+                    @Override
+                    public void execute(@NonNull Runnable command) {
+                        handler.post(command);
+                    }
+                };
+            }
+
+
+            BiometricPrompt biometricPrompt = new BiometricPrompt(getActivity(), executor, new BiometricPrompt.AuthenticationCallback() {
+                @Override
+                public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                    super.onAuthenticationError(errorCode, errString);
+                    if (errorCode == BiometricPrompt.ERROR_USER_CANCELED) {
+                        // Just negative button tap
+                        return;
+                    }
+                    Log.d(TAG,"Authentication error ...");
+                    new AlertDialog.Builder(getActivity())
+                            .setTitle("Error")
+                            .setMessage(errorCode + ": " + errString)
+                            .create()
+                            .show();
+                    Toast.makeText(getContext(), "Biometric Setting Failed", Toast.LENGTH_SHORT).show();
+                    buttonView.setChecked(false);
+                }
+
+                @Override
+                public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                    super.onAuthenticationSucceeded(result);
+                    Log.d(TAG,"Authentication Success ...");
+                    Toast.makeText(getContext(), "Biometric Setting Updated", Toast.LENGTH_SHORT).show();
+
+                    authenVal = isChecked;
+                    editor.putBoolean(getString(R.string.biometric_authentication),authenVal);
+                    Log.d(TAG,"Saving authentication preference: "+authenVal);
+                    editor.commit();
+                }
+
+                @Override
+                public void onAuthenticationFailed() {
+                    super.onAuthenticationFailed();
+                    Log.d(TAG,"Authentication failed ...");
+                    Toast.makeText(getContext(), "Biometric Authentication Failed", Toast.LENGTH_SHORT).show();
+                    buttonView.setChecked(false);
+                }
+            });
+
+            BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Unlock AudioRecorder.")
+                    .setDescription("Scan your biometric")
+                    .setDeviceCredentialAllowed(true)
+                    .setConfirmationRequired(true)
+                    .build();
+
+            biometricPrompt.authenticate(promptInfo);
         }
     }
 }
